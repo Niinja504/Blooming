373d43f204e6f1e22e6b6fb86374226317709688
Luis <20230352@ricaldone.edu.sv>
2024-07-05 08:38:42 +0200

    modificacion de interfaces3

--- app/src/main/java/BaseDeDatos/IngresarUserFirebase.kt
+++
@@ -1,9 +0,0 @@
-package proyecto.expotecnica.blooming.BaseDeDatos
-
-import modelo.ClaseConexion
-import java.sql.ResultSet
-import java.sql.Statement
-
-object IngresarUserFirebase {
-
-}
--- app/src/main/java/BaseDeDatos/UsuarioFirebase.kt
+++
@@ -1,9 +0,0 @@
-package proyecto.expotecnica.blooming.BaseDeDatos
-
-data class UsuarioFirebase(
-    val uuid: String,
-    val email: String,
-    val username: String,
-    val iv: String,
-    val profilePicture: String
-)
