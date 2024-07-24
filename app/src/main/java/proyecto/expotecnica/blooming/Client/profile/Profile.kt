24744b75e58c83d376c0e5a9cce355c8577f83b7
Fer <20200335@ricaldone.edu.sv>
2024-07-23 22:47:26 -0600

    ...

--- app/src/main/java/proyecto/expotecnica/blooming/Client/profile/Profile.kt
+++ app/src/main/java/proyecto/expotecnica/blooming/Client/profile/Profile.kt
@@ -1,18 +1,41 @@
 package proyecto.expotecnica.blooming.Client.profile
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import modelo.ImageUtils
 import proyecto.expotecnica.blooming.R
 import proyecto.expotecnica.blooming.Client.ImageViewModel
+import java.io.File
+import java.io.IOException
 
 class Profile : Fragment() {
     private val imageViewModel: ImageViewModel by activityViewModels()
+    private var currentPhotoPath: String? = null
+    private var selectedImageUri: Uri? = null
+    private lateinit var IMG_Perfil: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -25,7 +48,17 @@ class Profile : Fragment() {
     ): View {
         val root = inflater.inflate(R.layout.fragment_profile_client, container, false)
 
+        //Variables que se van a utilizar
         val IMGUser = root.findViewById<ImageView>(R.id.IMG_User_Profile_Client)
+        val IMG_Perfil = root.findViewById<ImageView>(R.id.IMG_User_Profile_Client)
+        val SubirFoto = root.findViewById<ImageView>(R.id.ic_SubirIMG_Profile_Client)
+        val CerrarS = root.findViewById<Button>(R.id.btn_CerrarSesion_Client)
+
+        SubirFoto.setOnClickListener{
+            mostrarDialogoSeleccionImagen()
+        }
+
+
 
         // Observar los cambios en imageUrl
         imageViewModel.imageUrl.observe(viewLifecycleOwner) { url ->
@@ -39,5 +72,115 @@ class Profile : Fragment() {
         }
 
         return root
+    }
+
+    private fun mostrarDialogoSeleccionImagen() {
+        val opciones = arrayOf("Cámara", "Galería")
+        AlertDialog.Builder(requireContext())
+            .setTitle("Seleccionar imagen")
+            .setItems(opciones) { dialog, which ->
+                when (which) {
+                    0 -> abrirCamara()
+                    1 -> abrirGaleria()
+                }
+            }
+            .show()
+    }
+
+    private fun abrirCamara() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            // El permiso no está concedido, solicitar el permiso
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION_Profile)
+        } else {
+            // El permiso está concedido, abrir la cámara
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            val photoFile: File? = try {
+                ImageUtils.createImageFile(requireContext())
+            } catch (ex: IOException) {
+                null
+            }
+            photoFile?.also {
+                currentPhotoPath = it.absolutePath
+                val photoURI: Uri = FileProvider.getUriForFile(
+                    requireContext(),
+                    "${requireContext().packageName}.fileprovider",
+                    it
+                )
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE_Profile)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CAMERA_PERMISSION_Profile -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // El permiso ha sido concedido, abrir la cámara
+                    abrirCamara()
+                } else {
+                    // El permiso ha sido denegado, mostrar un mensaje o manejarlo apropiadamente
+                    Toast.makeText(requireContext(), "Permiso de cámara denegado", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun abrirGaleria() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_IMAGE_PICK_Profile)
+        } else {
+            // Show an error message to the user
+            Toast.makeText(requireContext(), "No application available to pick an image", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                REQUEST_IMAGE_CAPTURE_Profile -> {
+                    // Handle camera capture result
+                    currentPhotoPath?.let {
+                        val fileUri = Uri.fromFile(File(it))
+                        selectedImageUri = fileUri // Update selectedImageUri with the camera URI
+                        Glide.with(requireContext())
+                            .load(fileUri)
+                            .apply(RequestOptions.circleCropTransform())
+                            .into(IMG_Perfil)
+                    }
+                }
+                REQUEST_IMAGE_PICK_Profile -> {
+                    // Handle gallery selection result
+                    val fileUri = data?.data
+                    selectedImageUri = fileUri // Update selectedImageUri with the gallery URI
+                    Glide.with(requireContext())
+                        .load(fileUri)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(IMG_Perfil)
+                }
+            }
+        }
+    }
+
+    private fun getBitmapFromUri(context: Context, uri: Uri): Bitmap {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(context.contentResolver, uri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+        }
+    }
+
+
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE_Profile = 1
+        private const val REQUEST_IMAGE_PICK_Profile = 2
+        private const val REQUEST_CAMERA_PERMISSION_Profile = 100
     }
 }
