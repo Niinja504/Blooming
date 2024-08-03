f2a58b9184a1b9e419c086d0c6fa146b2384d077
Fabi <sosorto72@gmail.com>
2024-08-02 20:31:59 -0600

    mapa parte 2

--- app/src/main/java/proyecto/expotecnica/blooming/Admin/add_shipping_cost/AddShippingCost.kt
+++ app/src/main/java/proyecto/expotecnica/blooming/Admin/add_shipping_cost/AddShippingCost.kt
@@ -1,14 +1,23 @@
 package proyecto.expotecnica.blooming.Admin.add_shipping_cost
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapFragment
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import proyecto.expotecnica.blooming.R
 
 class AddShippingCost : Fragment(), OnMapReadyCallback {
@@ -28,13 +37,47 @@ class AddShippingCost : Fragment(), OnMapReadyCallback {
         val root = inflater.inflate(R.layout.fragment_add_shipping_cost_admin, container, false)
         dialogView = root
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.MapFragment) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.MapFragment) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+        val mapOptionButton: ImageButton = root.findViewById(R.id.MapOptionsMenu)
+        val popupMenu = PopupMenu(requireContext(), mapOptionButton)
+        popupMenu.menuInflater.inflate(R.menu.bottom_map_options, popupMenu.menu)
+        //Bucle en el cual se le cambia el color a lso items
+        for (i in 0 until popupMenu.menu.size()) {
+            val item = popupMenu.menu.getItem(i)
+            val spanString = SpannableString(item.title.toString())
+            spanString.setSpan(ForegroundColorSpan(Color.BLACK), 0, spanString.length, 0)
+            item.title = spanString
+        }
+
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            changeMap(menuItem.itemId)
+            true
+        }
+
+        mapOptionButton.setOnClickListener {
+            popupMenu.show()
+        }
 
         return root
     }
 
+    private fun changeMap(itemId: Int) {
+        when (itemId) {
+            R.id.Normal -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            R.id.Hibrido -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_HYBRID
+            R.id.Satelite -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            R.id.Terreno -> mGoogleMap?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+        }
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
+        val cityLocation = LatLng(13.694048301077249, -89.2167184011952)
+        val zoomLevel = 11f
+
+        mGoogleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(cityLocation, zoomLevel))
+        mGoogleMap?.addMarker(MarkerOptions().position(cityLocation).title("San Salvador"))
     }
 }
